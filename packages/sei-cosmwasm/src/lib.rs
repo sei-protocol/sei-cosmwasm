6fa7f732cd92c15e0d9ef2c5cc8d2c66e483960d
larry <26318510+larry0x@users.noreply.github.com>
2022-07-14 18:30:09 +0100

    enable `sei` feature for wasm contracts

--- packages/sei-cosmwasm/src/lib.rs
+++ packages/sei-cosmwasm/src/lib.rs
@@ -15,8 +15,7 @@ pub use query::{
 };
 pub use route::SeiRoute;
 
-// TODO: properly support this requirement behavior in sei-chain
-// // This export is added to all contracts that import this package, signifying that they require
-// // "sei" support on the chain they run on.
-// #[no_mangle]
-// extern "C" fn requires_sei() {}
+// This export is added to all contracts that import this package, signifying that they require
+// "sei" support on the chain they run on.
+#[no_mangle]
+extern "C" fn requires_sei() {}
