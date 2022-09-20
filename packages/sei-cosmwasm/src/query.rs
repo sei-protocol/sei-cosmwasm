c99082920688ace98c784d08383377e6ff5e41a2
Cyson <cyson@Cysons-MBP.attlocal.net>
2022-09-20 13:56:31 -0700

    Remove Eq serde

--- packages/sei-cosmwasm/src/query.rs
+++ packages/sei-cosmwasm/src/query.rs
@@ -8,7 +8,7 @@ use crate::sei_types::OrderResponse;
 use crate::Order;
 
 /// SeiQueryWrapper is an override of QueryRequest::Custom to access Sei-specific modules
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, JsonSchema)]
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub struct SeiQueryWrapper {
     pub route: SeiRoute,
@@ -19,7 +19,7 @@ pub struct SeiQueryWrapper {
 impl CustomQuery for SeiQueryWrapper {}
 
 /// SeiQuery is defines available query datas
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, JsonSchema)]
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum SeiQuery {
     ExchangeRates {},
@@ -52,55 +52,55 @@ pub enum SeiQuery {
 }
 
 /// ExchangeRatesResponse is data format returned from OracleRequest::ExchangeRates query
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, JsonSchema)]
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct ExchangeRatesResponse {
     pub denom_oracle_exchange_rate_pairs: Vec<DenomOracleExchangeRatePair>,
 }
 
 /// OracleTwapsResponse is data format returned from OracleRequest::OracleTwaps query
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, JsonSchema)]
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct OracleTwapsResponse {
     pub oracle_twaps: Vec<OracleTwap>,
 }
 
 /// DexTwapsResponse is data format returned from DexTwaps query
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, JsonSchema)]
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct DexTwapsResponse {
     pub twaps: Vec<DexTwap>,
 }
 
 /// EpochResponse is data format returned from Epoch query
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, JsonSchema)]
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct EpochResponse {
     pub epoch: Epoch,
 }
 
 /// GetOrdersResponse is data format returned from GetOrders query
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, JsonSchema)]
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct GetOrdersResponse {
     pub orders: Vec<OrderResponse>,
 }
 
 /// GetOrderdByIdResponse is data format returned from GetOrderById query
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, JsonSchema)]
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct GetOrderByIdResponse {
     pub order: OrderResponse,
 }
 
 /// OrderSimulationResponse is data format returned from OrderSimulation query
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, JsonSchema)]
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct OrderSimulationResponse {
     pub executed_quantity: Decimal,
 }
 
 /// GetDenomFeeWhitelistResponse is data format returned from GetDenomFeeWhitelist query
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, JsonSchema)]
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct GetDenomFeeWhitelistResponse {
     pub creators: Vec<String>,
 }
 
 /// CreatorInDenomFeeWhitelistResponse is data format returned from CreatorInDenomFeeWhitelist query
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, JsonSchema)]
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct CreatorInDenomFeeWhitelistResponse {
     pub whitelisted: bool,
 }
