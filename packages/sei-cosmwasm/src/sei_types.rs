4160f3afccd9793352332c20bb70a730b6750e24
Cyson <cyson@Cysons-MBP.attlocal.net>
2022-10-06 16:58:56 -0700

    Add Fokmarketbyvalue order type

--- packages/sei-cosmwasm/src/sei_types.rs
+++ packages/sei-cosmwasm/src/sei_types.rs
@@ -17,6 +17,7 @@ pub enum OrderType {
     Market = 1,
     Liquidation = 2,
     Fokmarket = 3, // fill-or-kill market order
+    Fokmarketbyvalue = 4, // fill-or-kill market by value order
 }
 
 #[derive(Serialize_repr, Deserialize_repr, Copy, Clone, Debug, PartialEq, Eq, Hash, JsonSchema)]
