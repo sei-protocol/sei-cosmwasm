4ca64a438b99ee6f0b5064351f265ae00fcc8ecf
Cyson <cyson@Cysons-MBP.attlocal.net>
2022-10-10 10:05:49 -0700

    Fix fmt

--- packages/sei-cosmwasm/src/sei_types.rs
+++ packages/sei-cosmwasm/src/sei_types.rs
@@ -16,7 +16,7 @@ pub enum OrderType {
     Limit = 0,
     Market = 1,
     Liquidation = 2,
-    Fokmarket = 3, // fill-or-kill market order
+    Fokmarket = 3,        // fill-or-kill market order
     Fokmarketbyvalue = 4, // fill-or-kill market by value order
 }
 
