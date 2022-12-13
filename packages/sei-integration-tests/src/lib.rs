72e01fbeea04b3a7ad36d19bdfac1de99ac76108
Uday Patil <udpatil@gmail.com>
2022-12-12 20:56:01 -0800

    [deps] Fix integration tests to be dev deps and not build on wasm target (#53)
    * [deps] Fix integration tests to be dev deps and not build on wasm target
    
    * fix cargo toml

--- packages/sei-integration-tests/src/lib.rs
+++ packages/sei-integration-tests/src/lib.rs
@@ -1,5 +1,5 @@
 // Exposed for testing only
 // Both unit tests and integration tests are compiled to native code, so everything in here does not need to compile to Wasm.
-#[cfg(not(target_arch = "wasm32"))]
+#![cfg(not(target_arch = "wasm32"))]
 pub mod helper;
 pub mod module;
