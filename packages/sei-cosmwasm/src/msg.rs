84a064698bd107f53c0c20b562a18742e29415e6
_dssei_ <denys@seinetwork.io>
2024-04-15 08:28:26 -0700

    address more comments

--- packages/sei-cosmwasm/src/msg.rs
+++ packages/sei-cosmwasm/src/msg.rs
@@ -43,13 +43,15 @@ pub enum SeiMsg {
     },
     /// Calls EVM contract deployed  at `to` address with the given `data`.
     /// Calls EVM contract as if the contract's caller called it directly.
-    /// Please note that the CW contract has to be in allow list in order to execute delegate call.
+    /// Please note that the CW contract has to be in
+    /// [allow list](https://github.com/sei-protocol/sei-chain/blob/seiv2/x/evm/types/params.go#L142)
+    /// in order to execute delegate call.
     /// The EVM (Solidity) contract `msg.sender` in this case will be the callers address.
     DelegateCallEvm {
         /// The address of the EVM contract to call
         to: String,
-        /// Base64 encoded data to pass to the contract
-        data: String, // base64 encoded
+        /// Base64 encoded binary data to pass to the contract
+        data: String,
     },
     /// Calls EVM contract deployed at `to` address with specified `value` and `data`.
     /// The from address is the contract address of the contract executing the call.
@@ -60,8 +62,8 @@ pub enum SeiMsg {
         value: Uint128,
         /// The address of the EVM contract to call
         to: String,
-        /// Base64 encoded data to pass to the contract
-        data: String, // base64 encoded
+        /// Base64 encoded binary data to pass to the contract
+        data: String,
     },
 }
 
